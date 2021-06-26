4245f138201ecddf139bd4770858c012062c4e4a
Geno <santi-condori-huanca@hotmail.com>
2021-06-26 00:02:29 -0300

    modifiaciones para heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
+const por = process.env.port || 3000;
 const app = express();
 app.get('/',(req,res) => {
   res.send('Hello world');});
-  app.listen(3000);
+  app.listen(port);
